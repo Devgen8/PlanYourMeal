64cf82291b82ecc6d7adb013cb17de7306ae8435
Evgeniy Kamaev <zhecka.kamaev@yandex.ru>
2019-11-06 09:36:41 +0300

    Added prefetching and caching of images in Recipes

--- PlanYourMeal/Cells/RecipeCollectionViewCell.swift
+++ PlanYourMeal/Cells/RecipeCollectionViewCell.swift
@@ -12,11 +12,32 @@ class RecipeCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var image: UIImageView!
     
+    //@IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
     @IBOutlet weak var nameOfrecipe: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    func updateAppearanceFor(_ image: UIImage?) {
+        self.displayImage(image)
+    }
 
+    override func prepareForReuse() {
+        image.image = nil
+    }
+
+    private func displayImage(_ image: UIImage?) {
+        if let _ = image {
+            self.image.image = image
+            //loadingIndicator.stopAnimating()
+            //loadingIndicator.isHidden = true
+        } else {
+            self.image.image = nil
+            //loadingIndicator.startAnimating()
+            //loadingIndicator.isHidden = false
+        }
+    }
 }
